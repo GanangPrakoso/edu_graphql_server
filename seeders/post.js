60b6e72fc6446fa23d47bbaa9b1ff46e218c8811
GanangPrakoso <ganangmanggolo@gmail.com>
2024-01-03 01:31:47 +0700

    add seed data

---
+++ seeders/post.js
@@ -0,0 +1,20 @@
+const posts = [
+  {
+    userId: "1",
+    title: "Hari ini saya pergi tamasya",
+    caption: "lumayan seru",
+    tags: ["holiday", "fun"],
+  },
+  {
+    userId: "1",
+    title: "Hari ini saya ga kemana-mana walaupun libur",
+    caption: "karena saya sakit",
+    tags: ["holiday", "sick", "no_fun"],
+  },
+  {
+    userId: "1",
+    title: "Andai aku jadi semut",
+    caption: "penasaran aja",
+    tags: ["curious"],
+  },
+];
---
+++ seeders/user.js
@@ -0,0 +1,20 @@
+const users = [
+  {
+    username: "user1",
+    email: "user1@mail.com",
+    password: "user1",
+    address: {
+      street: "elm street",
+      number: 666,
+    },
+  },
+  {
+    username: "user2",
+    email: "user2@mail.com",
+    password: "user2",
+    address: {
+      street: "paper street",
+      number: 1537,
+    },
+  },
+];
